9dc7f73b39e5a7108580523238edce0c2681aa2f
Zhminko Roman <zhminkors@gmail.com>
2024-09-02 23:16:57 +0200

    feat: add suspense to the event cards

--- src/app/_components/EventWrapper.tsx
+++ src/app/_components/EventWrapper.tsx
@@ -1,5 +1,6 @@
 import { getUserEvents } from "@/src/actions/actions";
 import EventCard from "./EventCard";
+import { Suspense } from "react";
 
 export default async function EventWrapper() {
   const user = await getUserEvents();
@@ -8,14 +9,16 @@ export default async function EventWrapper() {
     <div className="grid grid-cols-[repeat(auto-fill,_350px)] gap-2">
       {user?.events?.map((hobbyEvent, index) => {
         return (
-          <EventCard
-            title={hobbyEvent.title}
-            key={`${hobbyEvent.id}`}
-            description={hobbyEvent.content}
-            userName={user.name}
-            userImage={user.image}
-            eventImage={hobbyEvent.eventImage}
-          />
+          <Suspense key={`${hobbyEvent.id}`} fallback={<p>...Loading...</p>}>
+            <EventCard
+              title={hobbyEvent.title}
+              key={`${hobbyEvent.id}`}
+              description={hobbyEvent.content}
+              userName={user.name}
+              userImage={user.image}
+              eventImage={hobbyEvent.eventImage}
+            />
+          </Suspense>
         );
       })}
     </div>
